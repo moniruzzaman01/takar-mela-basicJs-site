dc5183894fc175d00cbb3ec94ba335952b5749da
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-02-17 02:52:32 +0600

    almost done with error handle and other. need to make responsive and need to solve some error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,93 @@
-const totalExpensesField = document.getElementById('total-expenses')
-const balanceField = document.getElementById('balance')
+function getExpenses() {
+    // i don't find any benefit to make these parameter dynamic. thats why i don't try to make them dynamic
+    const foodAmount = parseFloat(document.getElementById('food').value)
+    const rentAmount = parseFloat(document.getElementById('rent').value)
+    const clothAmount = parseFloat(document.getElementById('clothes').value)
+    if (isNaN(foodAmount) || isNaN(rentAmount) || isNaN(clothAmount)) {
+        return error(1)
+    } else if (foodAmount < 0 || rentAmount < 0 || clothAmount < 0) {
+        error(3)
+    } else {
+        error()
+        return (foodAmount + rentAmount + clothAmount)
+    }
+}
 
-const calculateBtn = document.getElementById('calculate-btn').addEventListener('click', function () {
+function getSub(value1, value2) {
+    return (parseFloat(value1) - parseFloat(value2))
+}
+
+function error(value) {
+    const errorMessage1 = document.getElementById('message-1')
+    const errorMessage2 = document.getElementById('message-2')
+    const errorMessage3 = document.getElementById('message-3')
+    const errorMessage4 = document.getElementById('message-4')
+    if (value == 1) {
+        errorMessage1.style.display = 'block'
+        errorMessage2.style.display = 'none'
+        errorMessage3.style.display = 'none'
+        errorMessage4.style.display = 'none'
+    } else if (value == 2) {
+        errorMessage1.style.display = 'none'
+        errorMessage2.style.display = 'block'
+        errorMessage3.style.display = 'none'
+        errorMessage4.style.display = 'none'
+    } else if (value == 3) {
+        errorMessage1.style.display = 'none'
+        errorMessage2.style.display = 'none'
+        errorMessage3.style.display = 'block'
+        errorMessage4.style.display = 'none'
+    } else if (value == 4) {
+        errorMessage1.style.display = 'none'
+        errorMessage2.style.display = 'none'
+        errorMessage3.style.display = 'none'
+        errorMessage4.style.display = 'block'
+    } else {
+        errorMessage1.style.display = 'none'
+        errorMessage2.style.display = 'none'
+        errorMessage3.style.display = 'none'
+        errorMessage4.style.display = 'none'
+    }
+}
+
+const balance = document.getElementById('balance')
+document.getElementById('calculate-btn').addEventListener('click', function () {
     const income = document.getElementById('income').value
-    // add
-    const food = parseInt(document.getElementById('food').value)
-    const rent = parseInt(document.getElementById('rent').value)
-    const cloth = parseInt(document.getElementById('clothes').value)
-    const expenses=food+rent+cloth
-
-    // expenses set
-    totalExpensesField.innerText = expenses
-
-    // balance set
-    balanceField.innerHTML = income - expenses
+    const expenses = getExpenses()
+    if (income < expenses) {
+        error(2)
+    } else {
+        // expenses set
+        document.getElementById('total-expenses').innerText = expenses
+        // balance set
+        balance.innerText = getSub(income, expenses)
+    }
 })
 document.getElementById('savings-btn').addEventListener('click', function () {
     const income = document.getElementById('income').value
-
-    const savingsRateField = document.getElementById('savings')
     const savingAmountField = document.getElementById('saving-amount')
     const remainingBalanceField = document.getElementById('remaining-balance')
+    const savingsRate = parseInt(document.getElementById('savings').value)
+    if (isNaN(savingsRate)) {
+        error(1)
+    } else if (savingsRate < 0) {
+        error(3)
+    } else {
+        // savings count
+        const savingAmount = income * (savingsRate / 100)
+        // savings amount set
+        savingAmountField.innerText = savingAmount;
+        // remaining balance set
+        remainingBalanceField.innerText = getSub(balance.innerText, savingAmount)
+    }
 
-    const savingsRate = parseInt(savingsRateField.value)
-
-    const savingAmount = income * (savingsRate / 100)
-    savingAmountField.innerText = savingAmount;
-
-    remainingBalanceField.innerText = parseInt(balanceField.innerText) - savingAmount
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-const errorMessage1 = document.getElementById('message-1').innerText
-const errorMessage2 = document.getElementById('message-2').innerText
-const errorMessage3 = document.getElementById('message-3').innerText
+// clear the previous value of input
+const inputs = document.getElementsByTagName('input')
+for(const input of inputs)
+{
+    input.addEventListener('click',function(){
+        this.value=''
+    })
+}
